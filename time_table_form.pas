96b6c54613a4ca4b83821f03b382ce7c72e4afe1
Dmitry Shishimorov <illusorymbox@yandex.ru>
2014-04-29 14:31:49 +1100

    Доработка вывода расписания (оптимизирован код, добавлена кнопка расширения строки)

--- time_table_form.pas
+++ time_table_form.pas
@@ -7,12 +7,13 @@ interface
 uses
   Classes, SysUtils, db, sqldb, FileUtil, Forms, Controls, Graphics, Dialogs,
   Grids, ExtCtrls, StdCtrls, Menus, Buttons, search_frame, search_filter_frame,
-  metadata, data, math;
+  metadata, math, lazregions;
 
 type
 
   { TTimeTable }
 
+  TPoints = array of TPoint;
   TIntArray = array of integer;
 
   TTimeTable = class(TForm)
@@ -31,27 +32,34 @@ type
     TopPanel: TPanel;
     FilterPanel: TPanel;
     procedure AddFilterBtnClick(Sender: TObject);
+    procedure CheckFieldsItemClick(Sender: TObject; Index: integer);
     procedure CloseFilterClick(AFilterIndex: integer);
     procedure DrawGridDrawCell(Sender: TObject; aCol, aRow: Integer;
       aRect: TRect; aState: TGridDrawState);
+    procedure DrawGridMouseMove(Sender: TObject; Shift: TShiftState; X,
+      Y: Integer);
+    procedure DrawGridMouseUp(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
     procedure FormCreate(Sender: TObject);
     procedure HorizontalFieldBoxChange(Sender: TObject);
     procedure SelectBtnClick(Sender: TObject);
+    procedure OnPropChange(Sender: TObject);
     procedure VerticalFieldBoxChange(Sender: TObject);
   private
     procedure InitCheckFields;
     function GetTitles(AFieldIndex: integer; var AIDArray: TIntArray): TStringList;
     function GetTitlesQuery(AFieldIndex: integer): string;
-    procedure DrawTitles(ACol, ARow: integer; ARect: TRect);
+    function GetTriangle(ARect: TRect; ACol, ARow: integer): TPoints;
+    procedure DrawTitle(ACol, ARow: integer; ARect: TRect);
     procedure ClearCellData;
   private
-    FColTitles, RowTitles: TStringList;
+    FColTitles, FRowTitles: TStringList;
     FCellData: array of array of array of TStringList;
     FHFIDArray, FVFIDArray: TIntArray;
     FCurHFI, FCurVFI: integer;
     FTable, FMDTable: TTableInfo;
     FFilters: TFilters;
-    FRowHeight, FTextHeight: integer;
+    FDefaultRowHeight, FTextHeight: integer;
   const
     FieldSelectionError: string = 'Поле по горизонтали совпадает с полем по вертикали';
   end;
@@ -84,88 +92,130 @@ end;
 procedure TTimeTable.DrawGridDrawCell(Sender: TObject; aCol, aRow: Integer;
   aRect: TRect; aState: TGridDrawState);
 var
-  CurTop: integer;
-  i, j: integer;
+  tmp: string;
+  CurTop, i, j, k: integer;
 begin
+  with DrawGrid.Canvas do begin
+    Pen.Color := clBlack;
+    Pen.Style := psSolid;
+    Brush.Color := clBlack;
+    Brush.Style := bsClear;
+    Font.Color := clBlack;
+  end;
+
   if (aCol = 0) and (aRow = 0) then Exit;
   if (aCol = 0) xor (aRow = 0) then begin
-    DrawTitles(aCol, aRow, aRect);
+    DrawTitle(aCol, aRow, aRect);
     Exit;
   end;
 
   CurTop := aRect.Top;
   for i := 0 to high(FCellData[aCol][aRow]) do begin
+    k := 0;
     for j := 0 to FCellData[aCol][aRow][i].Count-1 do begin
-      DrawGrid.Canvas.TextOut(
-        aRect.Left+5, CurTop+(FTextHeight+4)*j+2, FCellData[aCol][aRow][i].Strings[j]);
+      if CheckFields.Checked[j] then begin
+        tmp := FCellData[aCol][aRow][i].Strings[j];
+        if ShowFieldNames.Checked then
+          tmp := Format('%s: %s', [FTable.Fields[j].Caption, tmp]);
+        DrawGrid.Canvas.TextOut(aRect.Left+5, CurTop+(FTextHeight+4)*k+2, tmp);
+        inc(k);
+      end;
     end;
-    CurTop += (FTextHeight+4)*FCellData[aCol][aRow][i].Count+1;
-    DrawGrid.Canvas.MoveTo(aRect.Left, CurTop);
-    DrawGrid.Canvas.LineTo(aRect.Right, CurTop);
+    if (k > 0) and (i < high(FCellData[aCol][aRow])) then begin
+      CurTop += (FTextHeight+4)*k+1;
+      DrawGrid.Canvas.MoveTo(aRect.Left, CurTop);
+      DrawGrid.Canvas.LineTo(aRect.Right, CurTop);
+    end;
   end;
+  if (Length(FCellData[aCol][aRow]) > 1) and
+     (DrawGrid.RowHeights[aRow] < FDefaultRowHeight*Length(FCellData[aCol][aRow]))
+  then begin
+    DrawGrid.Brush.Style := bsHorizontal;
+    DrawGrid.Brush.Color := clRed;
+    DrawGrid.Canvas.Polygon(GetTriangle(aRect, aCol, aRow));
+  end;
+end;
+
+procedure TTimeTable.DrawGridMouseMove(Sender: TObject; Shift: TShiftState; X,
+  Y: Integer);
+begin
+end;
+
+procedure TTimeTable.DrawGridMouseUp(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+begin
+  with DrawGrid do
+    if DrawGrid.RowHeights[Row] < FDefaultRowHeight*Length(FCellData[Col][Row]) then
+      if IsPointInPolygon(X, Y, GetTriangle(CellRect(Col, Row), Col, Row)) then
+        RowHeights[Row] := FDefaultRowHeight*Length(FCellData[Col][Row]);
 end;
 
 procedure TTimeTable.SelectBtnClick(Sender: TObject);
 var
-  FCounter, i, j, k: integer;
-  tmp: string;
+  FieldCounter, PrevCol, PrevRow, i: integer;
+  CurCol, CurRow: string;
+  CurCell: TStringList;
 begin
   FColTitles.Free;
   FColTitles := GetTitles(FCurHFI, FHFIDArray);
   DrawGrid.ColCount := FColTitles.Count+1;
 
-  RowTitles.Free;
-  RowTitles := GetTitles(FCurVFI, FVFIDArray);
-  DrawGrid.RowCount := RowTitles.Count+1;
+  FRowTitles.Free;
+  FRowTitles := GetTitles(FCurVFI, FVFIDArray);
+  DrawGrid.RowCount := FRowTitles.Count+1;
 
   ClearCellData;
   SetLength(FCellData, DrawGrid.ColCount);
-  SQLQuery.SQL := FMDTable.SQL;
-  for i := 1 to DrawGrid.ColCount-1 do begin
+  for i := 1 to high(FCellData) do
     SetLength(FCellData[i], DrawGrid.RowCount);
-    for j := 1 to DrawGrid.RowCount-1 do begin
-      SQLQuery.SQL.Strings[WHERE_IND] := Format('WHERE %s = %d and %s = %d',
-        [FTable.Fields[FCurHFI].Name, FHFIDArray[i-1],
-         FTable.Fields[FCurVFI].Name, FVFIDArray[j-1]]);
-      GetFiltersSQL(FFilters, SQLQuery);
-      //ShowMessage(SQLQuery.SQL.Text);
-      SQLQuery.Open;
-      while not SQLQuery.EOF do begin
-        SetLength(FCellData[i][j], Length(FCellData[i][j])+1);
-        FCellData[i][j][high(FCellData[i][j])] := TStringList.Create;
-        FCounter := 0;
-        for k := 1 to high(FMDTable.Fields) do begin
-          if (FMDTable.Fields[k].Name <> FTable.Fields[FCurHFI].Name) and
-             (FMDTable.Fields[k].Name <> FTable.Fields[FCurVFI].Name) then begin
-            if CheckFields.Checked[FCounter] then begin
-              tmp := SQLQuery.FieldByName(FMDTable.GetColumnName(k)).AsString;
-              if ShowFieldNames.Checked then
-                tmp := Format('%s: %s', [FMDTable.Fields[k].Caption, tmp]);
-              FCellData[i][j][high(FCellData[i][j])].Add(tmp);
-            end;
-            inc(FCounter);
-          end;
+
+  with SQLQuery do begin
+    SQL := FMDTable.SQL;
+    GetFiltersSQL(FFilters, SQLQuery);
+    SQL.Strings[ORDER_BY_IND] := Format('ORDER BY %s, %s',
+      [FTable.GetOrderByFieldName(FCurHFI), FTable.GetOrderByFieldName(FCurVFI)]);
+    Open;
+
+    PrevCol := 0;
+    PrevRow := 0;
+    while not EOF do begin
+      CurCell := TStringList.Create;
+      FieldCounter := 0;
+      for i := 1 to high(FMDTable.Fields) do begin
+        if FMDTable.Fields[i].Name = FTable.Fields[FCurHFI].Name then begin
+          CurCol := SQLQuery.FieldByName(FMDTable.GetColumnName(i)).AsString;
+          Continue;
         end;
-        //ShowMessage(FCellData[i][j][high(FCellData[i][j])].Text);
-        SQLQuery.Next;
+        if FMDTable.Fields[i].Name = FTable.Fields[FCurVFI].Name then begin
+          CurRow := SQLQuery.FieldByName(FMDTable.GetColumnName(i)).AsString;
+          Continue;
+        end;
+        CurCell.Add(SQLQuery.FieldByName(FMDTable.GetColumnName(i)).AsString);
       end;
-      FRowHeight := Max(FRowHeight, Length(FCellData[i][j]));
-      SQLQuery.Close;
+
+      if FColTitles.Strings[PrevCol] <> CurCol then begin
+        PrevRow := 0;
+        for i := PrevCol+1 to FColTitles.Count-1 do
+          if CurCol = FColTitles.Strings[i] then begin
+            PrevCol := i;
+            break;
+          end;
+      end;
+      if FRowTitles.Strings[PrevRow] <> CurRow then begin
+        for i := PrevRow+1 to FRowTitles.Count-1 do
+          if CurRow = FRowTitles.Strings[i] then begin
+            PrevRow := i;
+            break;
+          end;
+      end;
+      SetLength(FCellData[PrevCol+1][PrevRow+1], Length(FCellData[PrevCol+1][PrevRow+1])+1);
+      FCellData[PrevCol+1][PrevRow+1][high(FCellData[PrevCol+1][PrevRow+1])] := CurCell;
+      Next;
     end;
+    Close;
   end;
-
-  j := 0;
-  for i := 0 to high(FMDTable.Fields)-3 do
-    if CheckFields.Checked[i] then inc(j);
-  FTextHeight := DrawGrid.Canvas.TextHeight('А');
-  DrawGrid.DefaultRowHeight := FRowHeight*(FTextHeight+4)*j+j;
-  DrawGrid.DefaultColWidth := 250;
-  DrawGrid.RowHeights[0] := 30;
-  DrawGrid.ColWidths[0] := 150;
-
-  DrawGrid.Repaint;
+  OnPropChange(self);
 end;
-
 
 procedure TTimeTable.HorizontalFieldBoxChange(Sender: TObject);
 begin
@@ -215,11 +265,11 @@ begin
   SQLQuery.Close;
 end;
 
-procedure TTimeTable.DrawTitles(ACol, ARow: integer; ARect: TRect);
+procedure TTimeTable.DrawTitle(ACol, ARow: integer; ARect: TRect);
 begin
   with DrawGrid do begin
-    if aRow = 0 then Canvas.TextOut(ARect.Left+3, ARect.Top+5, FColTitles[aCol-1])
-    else Canvas.TextOut(ARect.Left+3, ARect.Top+5, RowTitles[aRow-1]);
+    if aRow = 0 then Canvas.TextOut(ARect.Left+5, ARect.Top+2, FColTitles[aCol-1])
+    else Canvas.TextOut(ARect.Left+5, ARect.Top+2, FRowTitles[aRow-1]);
   end;
 end;
 
@@ -237,7 +287,6 @@ end;
 procedure TTimeTable.ClearCellData;
 var i, j, k: integer;
 begin
-  FRowHeight := 0;
   for i := 0 to high(FCellData) do begin
     for j := 0 to high(FCellData[i]) do begin
       for k := 0 to high(FCellData[i][j]) do
@@ -263,6 +312,37 @@ procedure TTimeTable.CloseFilterClick(AFilterIndex: integer);
 begin
   FiltersBox.Height := FiltersBox.Height-FFilters[AFilterIndex].Height;
   CloseFilter(FiltersBox, FFilters, AFilterIndex);
+end;
+
+procedure TTimeTable.CheckFieldsItemClick(Sender: TObject; Index: integer);
+begin
+  OnPropChange(self);
+end;
+
+procedure TTimeTable.OnPropChange(Sender: TObject);
+var Count, i: integer;
+begin
+  Count := 0;
+  for i := 0 to high(FMDTable.Fields)-3 do
+    if CheckFields.Checked[i] then inc(Count);
+  FTextHeight := DrawGrid.Canvas.TextHeight('А');
+  FDefaultRowHeight := (FTextHeight+4)*Count+1;
+  DrawGrid.DefaultRowHeight := Max(FDefaultRowHeight, FTextHeight+4);
+  DrawGrid.DefaultColWidth := 250;
+  DrawGrid.RowHeights[0] := 30;
+  DrawGrid.ColWidths[0] := 150;
+  DrawGrid.Repaint;
+end;
+
+function TTimeTable.GetTriangle(ARect: TRect; ACol, ARow: integer): TPoints;
+begin
+  SetLength(Result, 3);
+  Result[0].X := ARect.Left+DrawGrid.ColWidths[ACol];
+  Result[0].Y := ARect.Top+DrawGrid.RowHeights[ARow]-20;
+  Result[1].X := ARect.Left+DrawGrid.ColWidths[ACol];
+  Result[1].Y := ARect.Top+DrawGrid.RowHeights[ARow];
+  Result[2].X := ARect.Left+DrawGrid.ColWidths[ACol]-20;
+  Result[2].Y := ARect.Top+DrawGrid.RowHeights[ARow];
 end;
 
 end.
